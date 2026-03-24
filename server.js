01b315daa4daef5bbaa0b121e5ef28a3e3b3bcca
Ishaan2105 <ishaanhingway@gmail.com>
2026-03-24 13:24:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -152,34 +152,34 @@ const { google } = require('googleapis');
 const OAuth2 = google.auth.OAuth2;
 
 const createTransporter = async () => {
-    const oauth2Client = new OAuth2(
-        process.env.GMAIL_CLIENT_ID,
-        process.env.GMAIL_CLIENT_SECRET,
-        "https://developers.google.com/oauthplayground"
-    );
+    try {
+        const oauth2Client = new OAuth2(
+            process.env.GMAIL_CLIENT_ID,
+            process.env.GMAIL_CLIENT_SECRET,
+            "https://developers.google.com/oauthplayground"
+        );
 
-    oauth2Client.setCredentials({
-        refresh_token: process.env.GMAIL_REFRESH_TOKEN
-    });
-
-    const accessToken = await new Promise((resolve, reject) => {
-        oauth2Client.getAccessToken((err, token) => {
-            if (err) reject("Failed to create access token");
-            resolve(token);
+        oauth2Client.setCredentials({
+            refresh_token: process.env.GMAIL_REFRESH_TOKEN
         });
-    });
 
-    return nodemailer.createTransport({
-        service: "gmail",
-        auth: {
-            type: "OAuth2",
-            user: process.env.EMAIL_USER,
-            accessToken,
-            clientId: process.env.GMAIL_CLIENT_ID,
-            clientSecret: process.env.GMAIL_CLIENT_SECRET,
-            refreshToken: process.env.GMAIL_REFRESH_TOKEN
-        }
-    });
+        const accessToken = await oauth2Client.getAccessToken();
+
+        return nodemailer.createTransport({
+            service: "gmail",
+            auth: {
+                type: "OAuth2",
+                user: process.env.EMAIL_USER,
+                accessToken: accessToken.token, // Ensure you use .token here
+                clientId: process.env.GMAIL_CLIENT_ID,
+                clientSecret: process.env.GMAIL_CLIENT_SECRET,
+                refreshToken: process.env.GMAIL_REFRESH_TOKEN
+            }
+        });
+    } catch (error) {
+        console.error("Transporter Creation Error:", error);
+        throw error;
+    }
 };
 
 // Forgot Password Route
